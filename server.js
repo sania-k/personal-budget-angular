0f716a7d0528aba0fe51ea05477444e3c78e0d40
sania-k <114709835+sania-k@users.noreply.github.com>
2024-09-15 15:05:41 -0400

    loading data from JSON file

--- server.js
+++ server.js
@@ -1,30 +1,18 @@
 const express = require('express');
+const budget = require('./budget.json');
 const app = express();
 const port = 3456;
 
 app.use('/', express.static('public'));
 
-const budget = [
-    {
-        title: 'Eat out',
-        budget: 70
-    },
-    {
-        title: 'Rent',
-        budget: 850
-    },
-    {
-        title: 'Groceries',
-        budget: 120
-    }
-]
+
 
 app.get('/hello', (req, res) => {
     res.send('Hello World! :D');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.json(budget)
 });
 
 
