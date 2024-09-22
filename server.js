c4f86debac8af024613f843ddcad57b2a9cf7dc0
sania-k <114709835+sania-k@users.noreply.github.com>
2024-09-22 15:19:48 -0400

    implemented cors

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const budget = require('./budget.json');
 const app = express();
 const port = 3456;
 
-app.use('/', express.static('public'));
-
-
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World! :D');
-});
+app.use(cors);
 
 app.get('/budget', (req, res) => {
     res.json(budget)
 });
 
 
-
 app.listen(port, () => {
-    console.log(`Example app listening @ http://localhost:${port}`)
+    console.log(`API served @ http://localhost:${port}`)
 });
